bf03d087791a72c34909c28c55e904ad1d32f459
Malmie <miechurska.m88@gmail.com>
2017-11-19 17:24:32 +0100

    js code changed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,9 @@ $(function(){
 	var carouselList = $("#carousel ul");
 	function changeSlide() {
 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);//zmiana zdjęcia
+		console.log('test')
 	}
+	
 	setInterval(changeSlide, 3000);
 
 	function moveFirstSlide () {
@@ -10,5 +12,7 @@ $(function(){
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
+		console.log(test2)
+
 	}
 });
